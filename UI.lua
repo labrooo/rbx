8df8c042ced5709acedff69105cdb2ce5f4abb1a
Labrooo <110684967+labrooo@users.noreply.github.com>
2026-01-03 20:08:13 +0100

    Implement Rayfield UI with configuration options
    Added Rayfield UI creation and configuration.

--- UI.lua
+++ UI.lua
@@ -1,3 +1,84 @@
 local function create()
-   print("It does worrk")
+   local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+   local Window = Rayfield:CreateWindow({
+      Name = "Rayfield Example Window",
+      Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+      LoadingTitle = "Rayfield Interface Suite",
+      LoadingSubtitle = "by Sirius",
+      ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
+      Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+   
+      ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+   
+      DisableRayfieldPrompts = false,
+      DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   
+      ConfigurationSaving = {
+         Enabled = true,
+         FolderName = nil, -- Create a custom folder for your hub/game
+         FileName = "Big Hub"
+      },
+   
+      Discord = {
+         Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+         Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+         RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      },
+   
+      KeySystem = false, -- Set this to true to use our key system
+      KeySettings = {
+         Title = "Untitled",
+         Subtitle = "Key System",
+         Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+         FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+         SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+         Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      }
+   })
+   
+   local Tab = Window:CreateTab("Movement", "rewind")
+   local Section = Tab:CreateSection("Speed Configuration")
+   local Toggle = Tab:CreateToggle({
+      Name = "Toggle Speed",
+      CurrentValue = false,
+      Flag = "ToggleSpeedFlag", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+      Callback = function(Value)
+      -- The function that takes place when the toggle is pressed
+      -- The variable (Value) is a boolean on whether the toggle is true or false
+      end,
+   })
+   local Dropdown = Tab:CreateDropdown({
+      Name = "Execute on",
+      Options = {"Hold","Press"},
+      CurrentOption = {"Hold"},
+      MultipleOptions = false,
+      Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+      Callback = function(Options)
+      -- The function that takes place when the selected option is changed
+      -- The variable (Options) is a table of strings for the current selected options
+      end,
+   })
+   local Keybind = Tab:CreateKeybind({
+      Name = "Keybind",
+      CurrentKeybind = "Q",
+      HoldToInteract = false,
+      Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+      Callback = function(Keybind)
+      -- The function that takes place when the keybind is pressed
+      -- The variable (Keybind) is a boolean for whether the keybind is being held or not (HoldToInteract needs to be true)
+      end,
+   })
+   local Slider = Tab:CreateSlider({
+      Name = "Speed",
+      Range = {0, 1000},
+      Increment = 1,
+      Suffix = "",
+      CurrentValue = 16,
+      Flag = "SpeedValueSlider", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+      Callback = function(Value)
+      -- The function that takes place when the slider changes
+      -- The variable (Value) is a number which correlates to the value the slider is currently at
+      end,
+   })
 end
