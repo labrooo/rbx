d8b59ed3a062b0d93f76bb599bda2ee2d8dba626
Labrooo <110684967+labrooo@users.noreply.github.com>
2026-01-03 20:09:40 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
---local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/labrooo/rbx/refs/heads/main/UI.lua'))()
-
-
 -- SCRIPTS
 local MovementScripts = loadstring(game:HttpGet('https://raw.githubusercontent.com/labrooo/rbx/refs/heads/main/scripts/Movement.lua'))()
 
 
+-- AFTER ALL SCRIPTS LOADED, INCLUDING SETTINGS:
+local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/labrooo/rbx/refs/heads/main/UI.lua'))()
+UI.create() -- Opens the UI when injected
