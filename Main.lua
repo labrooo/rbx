465b3e34403131f74d82b1ffc8e03978801f5ef0
Labrooo <110684967+labrooo@users.noreply.github.com>
2026-01-03 20:00:52 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,7 @@
-local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/labrooo/rbx/refs/heads/main/UI.lua'))()
-UI.create()
+--local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/labrooo/rbx/refs/heads/main/UI.lua'))()
+
+
+-- SCRIPTS
+local MovementScripts = loadstring(game:HttpGet('https://raw.githubusercontent.com/labrooo/rbx/refs/heads/main/scripts/Movement.lua'))()
+
 
